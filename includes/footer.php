fe1a19867be7592745b7976b0b525d7809923d51
Andres Felipe <andresalapeq@gmail.com>
2026-06-09 07:44:13 -0500

    el footer

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<footer class="footer text-center py-3 mt-auto">
+        <div class="container">
+            <p class="mb-0">&copy; <?= date('Y') ?> Sistema de Control de Asistencia | Panel de Gestión</p>
+            <small class="text-center py-3 mt-auto">Desarrollado en PHP Puro y MySQL</small>
+        </div>
+</footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
